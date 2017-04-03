73cbb00dfed8a92d939b96c898a72a86ebb3d264
Bob Specht <bspcht@gmail.com>
2017-04-02 22:48:25 -0500

    Initial HMTL/CSS views
    Welcome and Home views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>teem</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Dosis:400,700" rel="stylesheet">
 
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
-</head>
-<body>
-    <div id="app">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script>
+            window.Laravel = {!! json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!};
+        </script>
+    </head>
+    <body>
+      <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
@@ -36,22 +40,22 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        teem
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
+                    <!-- <ul class="nav navbar-nav">
                         &nbsp;
-                    </ul>
+                    </ul> -->
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">Sign in</a></li>
+                            <li><a href="{{ route('register') }}">Sign up</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -63,7 +67,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Sign out
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -80,7 +84,13 @@
 
         @yield('content')
     </div>
-
+    <footer>
+      <div class="container">
+        <div class="row footer-inner">
+          <span><a href="mailto:">Contact</a></span><span>&copy; 2017</span>
+        </div>
+      </div>
+    </footer>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,125 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+      <title>teem</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+      <link href="https://fonts.googleapis.com/css?family=Dosis:400,700" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      <!-- Styles -->
+      <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+      <!-- Scripts -->
+      <script>
+          window.Laravel = {!! json_encode([
+              'csrfToken' => csrf_token(),
+          ]) !!};
+      </script>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+    <body class="welcome">
+      <nav class="navbar navbar-default navbar-static-top">
+          <div class="container">
+              <div class="navbar-header">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                  <!-- Collapsed Hamburger -->
+                  <!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                      <span class="sr-only">Toggle Navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>-->
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                  <!-- Branding Image -->
+                  <a class="navbar-brand" href="{{ url('/') }}">
+                      teem
+                  </a>
+              </div>
+              
+              <!-- Right Side Of Navbar -->
+              <ul class="nav navbar-nav navbar-right">
+                  <!-- Authentication Links -->
+                  @if (Auth::guest())
+                      <li><a href="{{ route('login') }}">Sign in</a></li>
+                      <li><a href="{{ route('register') }}">Sign up</a></li>
+                  @else
+                      <li><a href="{{ url('/home') }}">Dashboard</a></li>
+                  @endif
+              </ul>
+          </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="welcome-title">
+              Your people, in sync.
             </div>
+            <div class="welcome-status-box">
+              <div class="status-box-titles">
+                <div class="status-box-titles_time">
+
+                </div>
+                <div class="status-box-titles-title">
+                  Name
+                </div>
+                <div class="status-box-titles-title">
+                  Status
+                </div>
+                <div class="status-box-titles-title">
+                  Until
+                </div>
+                <div class="status-box-titles-title status-box-titles-title-action">
+
+                </div>
+              </div>
+              <div class="status-box">
+                <div class="status-box_time">
+                  <div class="status-box-section-content">
+                    15m
+                  </div>
+                </div>
+                <div class="status-box-section status-box_name">
+                  <div class="status-box-section-content">
+                    <div class="status-box_name-first">Ella</div>
+                    <div class="status-box_name-last">Young</div>
+                  </div>
+                </div>
+                <div class="status-box-section status-box_status status-box_status-in">
+                  <div class="status-box-section-content">
+                    In
+                  </div>
+                </div>
+                <div class="status-box-section status-box_until">
+                  <div class="status-box-section-content">
+                    All Day
+                  </div>
+                </div>
+                <div class="status-box-section status-box_action">
+                  <div class="status-box-section-content">
+                    Ask
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="welcome-button">
+              <a href="/register" class="btn btn-primary btn-lg" role="button">Get Started</a>
+            </div>
+          </div>
         </div>
+      </div>
+      <footer>
+        <div class="container">
+          <div class="row footer-inner">
+            <span><a href="mailto:">Contact</a></span><span>&copy; 2017</span>
+          </div>
+        </div>
+      </footer>
     </body>
 </html>
