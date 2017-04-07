8674307f3ae82dfc32f9957730e75e7216294e7a
Chris Piggott <cpiggott@ksu.edu>
2017-04-07 00:24:42 -0500

    Added StatusType

---
+++ app/StatusType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class StatusType extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'label', 'color',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+
+    protected $dates = [
+        'deleted_at'
+    ];
+}
---
+++ database/migrations/2017_04_07_051434_create_status_types.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('label');
+            $table->string('color', 7);
+            $table->softDeletes();
+        });
+
+        DB::table('status_types')->insert([
+            'label' => 'In',
+            'color' => '#76e076'
+        ]);
+
+        DB::table('status_types')->insert([
+            'label' => 'Out',
+            'color' => '#eeb4b4'
+        ]);
+
+        DB::table('status_types')->insert([
+            'label' => 'Meeting',
+            'color' => '#f8e1e1'
+        ]);
+
+        DB::table('status_types')->insert([
+            'label' => 'Home',
+            'color' => '#b4eeb4'
+        ]);
+
+        DB::table('status_types')->insert([
+            'label' => 'None',
+            'color' => '#fafafa'
+        ]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('status_types');
+    }
+}
