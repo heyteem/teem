fa32794991e354d7d710d39b2d8e2704bf78e634
Chris Piggott <cpiggott@ksu.edu>
2017-04-06 23:24:14 -0500

    Added check for users teems

--- app/User.php
+++ app/User.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -34,4 +37,5 @@ class User extends Authenticatable
     public function teems(){
         return $this->belongsToMany('App\Teem');
     }
+
 }
