c5755123062b3e703585a9c51fbb85df7ffd3d75
Chris Piggott <cpiggott@ksu.edu>
2017-04-06 22:35:08 -0500

    Added organization basics

---
+++ app/Organization.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'user_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    public function creator(){
+        return $this->belongsTo('App\User');
+    }
+
+}
